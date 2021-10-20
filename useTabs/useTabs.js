8fd4e1eaa9ffdfc53f870f3deffe070a7e1ac5d9
NamgyungKim <namgyung.kim@gmail.com>
2021-10-20 15:22:57 +0900

    add export

--- useTabs/useTabs.js
+++ useTabs/useTabs.js
@@ -1,4 +1,4 @@
-const useTabs = (initialTab, allTabs) => {
+export const useTabs = (initialTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initialTab);
   if (!allTabs || !Array.isArray(allTabs)) {
     //allTabs가 비어있거나, Array가 아니면 return
